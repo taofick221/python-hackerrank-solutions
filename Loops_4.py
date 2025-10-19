01ceaff964d705f8d761809285d04ad7c07ec2ba
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-10-19 18:25:14 +0600

    python hackerrank code

---
+++ Loops_4.py
@@ -0,0 +1,10 @@
+# Task
+# The provided code stub reads an integer,n, from STDIN. For all non-negative integers ,i<n,print i^2
+
+# Example n=3
+
+# The list of non-negative integers that are less than n=3 . Print the square of each number on a separate line.
+
+n=int(input())
+for i in range(n):
+    print(i*i)
