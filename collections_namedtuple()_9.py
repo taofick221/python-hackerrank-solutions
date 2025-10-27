f8958afb0c0c02e714b9deeebe627de3a2cad44a
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-10-27 18:16:47 +0600

    python hackerrank code

--- collections_namedtuple()_9.py
+++ collections_namedtuple()_9.py
@@ -42,3 +42,28 @@ for _ in range(N):
         top_name = person.NAME
 
 print(top_name, top_marks)
+
+
+
+# You are given student data.
+
+# First line contains N → number of students.
+
+# Second line contains column names (for example: ID NAME CLASS MARKS) — order can be random.
+
+# Next N lines contain each student’s data in the same order as the column names.
+
+# Task:
+# Print the NAME and MARKS of the student who scored the highest MARKS.
+N = int(input())
+fields = input().split()
+
+students = []
+for _ in range(N):
+    data = input().split()
+    student = dict(zip(fields, data))  # Create key:value dictionary
+    students.append(student)
+
+top_student = max(students, key=lambda x: int(x["MARKS"]))
+
+print(top_student["NAME"], top_student["MARKS"])
